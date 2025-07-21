2b0a802ec89726ca3015e4256d888005777f49db
Endri Grigoryan <enik.grigoryan@yandex.ru>
2025-07-21 19:20:59 +0300

    fix: base path for Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import React, { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
 import './index.css';
 import { LoadingFallback } from './components/LoadingFallback/LoadingFallback';
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AuthProvider>
         <Suspense fallback={<LoadingFallback />}>
           <App />
         </Suspense>
       </AuthProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode >
 );
